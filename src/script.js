b1bedb0bcae42e12b980754f29ac223250c3516d
muhammad fiqi <muhammadfiqi85@gmail.com>
2026-01-09 14:34:06 +0700

    Refactor code structure for improved readability and maintainability

--- src/script.js
+++ src/script.js
@@ -1,13 +1,34 @@
-        tailwind.config = {
-            theme: {
-                extend: {
-                    fontFamily: {
-                        sans: ['Inter', 'sans-serif'],
-                    },
-                    colors: {
-                        
-                        'charcoal': '#121212',
-                    }
-                }
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                
+                'charcoal': '#121212',
             }
         }
+    }
+}
+
+$('.btn').on('click', function () {
+  const target = $(this).data('target');
+
+  if ($(this).hasClass('active')) return;
+
+  $('.btn').removeClass('active');
+  $(this).addClass('active');
+
+  $('.box:visible')
+    .stop(true, true)
+    .slideUp(250)
+    .promise()
+    .done(() => {
+      $(target).stop(true, true).slideDown(300);
+    });
+});
+$('.close-btn').on('click', function () {
+    $('.btn').removeClass('active');
+    $('.box:visible').slideUp(250);
+})
